8cf7cd7bcf0d9fd7807c2fdf951ba835eb3eafc6
pinejh <pinejh95@gmail.com>
2018-02-20 11:26:21 -0500

    only controlled by lmb

--- fishing-game/js/controlHandler.js
+++ fishing-game/js/controlHandler.js
@@ -1,8 +1,8 @@
 var m1 = false;
 
-document.body.onmousedown = function() {
-  m1 = true;
+document.body.onmousedown = function(e) {
+  if(e.button == 0) m1 = true;
 };
-document.body.onmouseup = function() {
-  m1 = false;
+document.body.onmouseup = function(e) {
+  if(e.button == 0) m1 = false;
 };
